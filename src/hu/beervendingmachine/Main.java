1071e4d46528b5976372e0ae3a0991402a77a32f
MiklosArpad <43209274+MiklosArpad@users.noreply.github.com>
2020-11-26 16:11:31 +0100

    Update Main.java

--- src/hu/beervendingmachine/Main.java
+++ src/hu/beervendingmachine/Main.java
@@ -1,7 +1,20 @@
 package hu.beervendingmachine;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("***** Sör automata v1.0 *****");
+
+        System.out.println("Válasszon az alábbi sörök közül");
+        List<String> sorok = new ArrayList<>
+                (Arrays.asList("Ászok", "Dreher", "Barna", "Ipa", "Szűretlen", "Vörös", "Cseh", "Belga"));
+
+        for (String sor : sorok)
+            System.out.println(sor);
+
+        System.out.println("Adja meg a kívánt mennyiséget!");
+
     }
 }
