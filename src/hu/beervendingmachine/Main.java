d1b13a31b6c6267729be8aa07b57955e3736a450
MiklosArpad <43209274+MiklosArpad@users.noreply.github.com>
2020-12-13 16:00:53 +0100

    sörök kiíratásának objektumokon keresztül

--- src/hu/beervendingmachine/Main.java
+++ src/hu/beervendingmachine/Main.java
@@ -1,47 +1,21 @@
 package hu.beervendingmachine;
 
-import java.util.*;
-
 public class Main {
-
-    private static ArrayList<String> getBeers() {
-        return new ArrayList<>
-                (Arrays.asList("Ászok", "Dreher", "Barna", "Ipa", "Szűretlen", "Vörös", "Cseh", "Belga"));
-    }
-
-    private static ArrayList<Integer> getPrices() {
-        return new ArrayList<>
-                (Arrays.asList(250, 270, 310, 330, 400, 370, 450, 780));
-    }
-
-    private static ArrayList<Integer> getCodes() {
-        return new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
-    }
-
-    private static ArrayList<Integer> getQuantities() {
-        return new ArrayList<>(Arrays.asList(100, 100, 100, 100, 100, 100, 100, 100));
-    }
 
     public static void main(String[] args) {
         Console.printHeader();
 
-        List<String> beers = getBeers();
-        List<Integer> prices = getPrices();
-        List<Integer> codes = getCodes();
-        List<Integer> quantities = getQuantities();
+        BeerRepository beerRepository = new BeerRepository();
 
         Integer ar = 0;
 
         while (true) {
             Console.writeLine("Válasszon az alábbi sörök közül:");
 
-            // Sörök kiíratása
-            for (int i = 0; i < 8; i++)
-                Console.writeLine(codes.get(i) + " " + beers.get(i) + " " +
-                        prices.get(i) + " Forint" + " " + quantities.get(i) + " db");
-
-            // A sörök kódjainak és darabszámának bekérése
-
+            for (Beer beer : beerRepository.getBeers()) {
+                Console.writeLine(beer.getCode() + " " + beer.getName() + " " +
+                        beer.getPrice() + " Forint" + " " + beer.getQuantity() + " db");
+            }
 
             Console.write("Adja meg a kívánt kódot: ");
             int kod = Console.readInNumber();
@@ -70,7 +44,7 @@ public class Main {
 
             Integer darabszamTemp = quantities.get(actualIndex);
             Integer darabszam = quantities.get(actualIndex) - mennyiseg;
-            quantities.remove(actualIndex);
+            quantities.remove(actualIndex); // TODO: decrementQuantity()
             quantities.add(actualIndex, darabszam);
 
             Console.writeLine("Sör kiadva...");
@@ -83,27 +57,5 @@ public class Main {
 
             Console.writeLine("Végösszeg: " + ar + ".- Ft\n");
         }
-    }
-
-    // Függvény szignatúra (feje), {} -> törzse: ide kerülhet algorimtus
-    // private, default, public, protected -> access modifier (láthatósági módosítani)
-    // static, abstract, final ... -> modifier (módosító)
-    // calculateTotalPrice -> függvény neve
-    // () -> paraméterlista
-    // (int price, int quantity) -> paraméter(ek)
-    // BEST PRACTISE:
-    // ideális állapot: nincs paraméter
-    // 1 paraméter OK!
-    // 2 paraméter még mindig OK!
-    // 3 paraméter na ez már necces!
-    // 4 ... n NA ILYET NE!
-    // SINGLE RESPONSIBILITY PRINCIPLE -> 1 osztály, 1 metódus 1 dolgot csináljon!
-    // boolean típusú paraméter -> GYANÚS (flag)
-
-    private static int calculateTotalPrice(int quantity, int price) {
-        return quantity * price;
-
-        // int total = quantity * price;
-        // return total;
     }
 }
